5c8dce04b9b150f48ea85befd51b2069a7be762f
fkatsuhiro <nakayoshiryutokatsu@gmail.com>
2025-12-11 19:51:21 +0900

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://fkatsuhiro.github.io',
   base: '/portfolio', 
   
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
